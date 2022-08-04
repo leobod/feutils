d769a1479341babf6c1f044a6bd8f90cac56f94f
leobod <leobod@dingtalk.com>
2022-08-04 12:26:25 +0800

    feat: given used schedule, mark the daterange which time has been used

--- src/demo/timeUsed.ts
+++ src/demo/timeUsed.ts
@@ -2,7 +2,10 @@
 
 interface TimeItemModel {
   label: string,
-  value: string
+  value: string,
+  status: string,
+  create: boolean,
+  modify: boolean
 }
 
 interface UsedModel {
@@ -16,15 +19,17 @@ interface TimingUsedModel {
   TStart: string,
   TEnd: string,
   NStart: number,
-  NDuration: number
+  NEnd: number,
+  NDuration: number,
+  NType: number, // 今天(0)  昨天(-1)  明天(1)  贯穿一整天(2)
 }
 
 
 const usedTimeList: Array<UsedModel> = [
-  {SName: '001', TStart: '2022-07-15 01:00:00', TEnd: '2022-07-15 02:15:00'},
-  {SName: '002', TStart: '2022-07-15 04:30:00', TEnd: '2022-07-15 04:45:00'},
-  {SName: '003', TStart: '2022-07-14 18:00:00', TEnd: '2022-07-15 01:00:00'},
-  {SName: '004', TStart: '2022-07-15 19:15:00', TEnd: '2022-07-16 01:00:00'},
+  {SName: '001', TStart: '2022-08-04 01:00:00', TEnd: '2022-08-04 02:15:00'},
+  {SName: '002', TStart: '2022-08-04 04:30:00', TEnd: '2022-08-04 04:45:00'},
+  {SName: '003', TStart: '2022-08-03 18:00:00', TEnd: '2022-08-04 01:00:00'},
+  {SName: '004', TStart: '2022-08-04 19:15:00', TEnd: '2022-08-05 01:00:00'},
 ];
 
 const TIME_STEP = 15;
@@ -46,7 +51,10 @@ function getTimeOptions(step: number): Array<TimeItemModel> {
       const time_minute = minute < 9 ? `0${minute}` : minute;
       timeList.push({
         label: `${time_hour}:${time_minute}`,
-        value: `${time_hour}:${time_minute}`
+        value: `${time_hour}:${time_minute}`,
+        status: '',
+        create: true,
+        modify: false
       })
     }
   }
@@ -61,13 +69,32 @@ function getTimeOptions(step: number): Array<TimeItemModel> {
 function timingUsedTimeList (opts: Array<UsedModel>): Array<TimingUsedModel> {
   const result : Array<TimingUsedModel> = []
   for (const item of opts) {
+    /* 转换为时间错，用于计算差值 */
     const start_timestamp = new Date(item.TStart).getTime()
     const end_timestamp = new Date(item.TEnd).getTime()
-    console.log((end_timestamp - start_timestamp) / 1000);
+    const today_last = new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1;
+    const today_first = new Date(new Date().toLocaleDateString()).getTime();
+    // console.log((end_timestamp - start_timestamp) / 1000);
+    let nType = 0
+    /* 是否是今天、明天、昨天... */
+    const isYesterday = today_first - start_timestamp > 0
+    const isTomorrow = today_last - end_timestamp < 0
+    if (!isYesterday && !isTomorrow) {
+      nType = 0
+    } else if (isYesterday && !isTomorrow) {
+      nType = -1
+    } else if (!isYesterday && isTomorrow) {
+      nType = 1
+    } else {
+      nType = 2
+    }
+    /* 记录 */
     result.push({
       ...item,
       NStart: start_timestamp,
+      NEnd: end_timestamp,
       NDuration: end_timestamp - start_timestamp,
+      NType: nType
     })
   }
   return result
@@ -91,20 +118,59 @@ function sortUsedTimeList (opts: Array<TimingUsedModel>): Array<TimingUsedModel>
  * @param opts
  * @param usedList
  */
-function mathUsedOptions (opts: Array<TimeItemModel>, usedList: Array<TimingUsedModel>):void {
+function mathUsedOptions (opts: Array<TimeItemModel>, usedList: Array<TimingUsedModel>): Array<TimeItemModel> {
   const imut_opts = JSON.parse(JSON.stringify(opts));
-  let pointer = 0;
-  const today_start = new Date(`2022/07/15 00:00:00`).getTime();
+  console.log(imut_opts)
+
+  console.log(usedList)
+  const today_start = new Date(new Date().toLocaleDateString()).getTime();
+  const unit_step = TIME_STEP * 60 * 1000
   for (const item of usedList) {
-    const diff_minutes = (item.NStart - today_start) / 1000 / 60
-    if (diff_minutes < 0) {
-
-    } else {
-
+    if (item.NType === -1) {
+      const diff_end_minutes = Math.ceil((item.NEnd - today_start));
+      const effect_piece = Math.ceil(diff_end_minutes / unit_step)
+      for (let i=0; i< effect_piece  && i < opts.length; ++i) {
+        console.log(i)
+        if (!imut_opts[i].modify) {
+          imut_opts[i].status = '(已使用)'
+          imut_opts[i].modify = true
+        }
+      }
+    } else if (item.NType === 0) {
+      const diff_start_minutes = Math.ceil((item.NStart - today_start));
+      const effect_start = Math.ceil(diff_start_minutes / unit_step);
+      const effect_piece = Math.ceil(item.NDuration / unit_step);
+      for (let i=effect_start; i< effect_start + effect_piece && i < opts.length; ++i) {
+        console.log(i)
+        console.log(item.TStart)
+        if (!imut_opts[i].modify) {
+          imut_opts[i].status = '(已使用)'
+          imut_opts[i].modify = true
+        }
+      }
+    } else if (item.NType === 1) {
+      const diff_start_minutes = Math.ceil((item.NStart - today_start));
+      const effect_start = Math.ceil(diff_start_minutes / unit_step);
+      for (let i=effect_start; i < opts.length; ++i) {
+        if (!imut_opts[i].modify) {
+          imut_opts[i].status = '(已使用)'
+          imut_opts[i].modify = true
+        }
+      }
+    } else if (item.NType === 2) {
+      for (let i=0; i < opts.length; ++i) {
+        if (!imut_opts[i].modify) {
+          imut_opts[i].status = '(已使用)'
+          imut_opts[i].modify = true
+        }
+      }
     }
-
   }
+  return imut_opts
 }
+
+// 数据测试当天为 2022-08-05
+console.log(mathUsedOptions(getTimeOptions(TIME_STEP), sortUsedTimeList(timingUsedTimeList(usedTimeList))));
 
 
 
