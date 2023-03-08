d9eba68f1507714fa1b7e5fdd057185bba453acb
leobod <leobod@dingtalk.com>
2023-03-08 11:08:02 +0800

    feat: JEST使用范例

---
+++ test/date.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from '@jest/globals'
+import { formateDate } from '../src/Date'
+
+describe('Date', () => {
+  test('formateDate', () => {
+    expect(formateDate('2023-03-08 11:07:00', 'YYYY/MM/DD')).toBe('2023/03/08')
+  })
+})
